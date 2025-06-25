fd4113068acf273f116e5fac69999f870ef6a501
Filipe <filipelehmannp@gmail.com>
2025-06-25 00:22:34 -0300

    update: even better formatting experience overview

--- components/ExperienceOverview.jsx
+++ components/ExperienceOverview.jsx
@@ -52,46 +52,49 @@ const ExperienceDetails = ({ index, jobs }) => {
     colors.secondary.dark
   );
   return (
-    <Grid
-      container
-      direction="column"
-      style={{
-        // maxWidth: { base: '90vw', sm: '70vw', md: '30vw' },
-        margin: 'auto',
-        padding: { base: '8px', sm: '12px' },
-      }}
-    >
-      <Grid item>
-        <Heading as="h1" size="md" textAlign={{ base: 'center', sm: 'left' }}>
-          {job.position} @{' '}
-          <Link href={job.url} isExternal color={secondary}>
-            {job.workplace}
-          </Link>
-        </Heading>
-      </Grid>
-      <Grid item textAlign={{ base: 'center', sm: 'left' }}>
-        {job.duration.map((duration) => (
-          <Text key={duration} mt={2}>
-            {duration}
-          </Text>
-        ))}
-      </Grid>
+    <Container maxWidth="680px" style={{ padding: 0 }}>
       <Grid
-        item
+        container
+        direction="column"
         style={{
-          textAlign: { base: 'left', sm: 'justify' },
-          paddingX: { base: '4px', sm: '0' },
+          margin: 'auto',
+          padding: { base: '8px', sm: '12px' },
         }}
       >
-        <UnorderedList mt={2}>
-          {job.description.map((desc) => (
-            <ListItem key={desc} style={{ wordBreak: 'break-word' }}>
-              {desc}
-            </ListItem>
+        <Grid item>
+          <Heading as="h1" size="md" textAlign={{ base: 'center', sm: 'left' }}>
+            {job.position} @{' '}
+            <Link href={job.url} isExternal color={secondary}>
+              {job.workplace}
+            </Link>
+          </Heading>
+        </Grid>
+        <Grid item textAlign={{ base: 'center', sm: 'left' }}>
+          {job.duration.map((duration) => (
+            <Text key={duration} mt={2}>
+              {duration}
+            </Text>
           ))}
-        </UnorderedList>
+        </Grid>
+        <Container maxWidth="680px">
+          <Grid
+            item
+            style={{
+              textAlign: { base: 'left', sm: 'justify' },
+              paddingX: { base: '4px', sm: '0' },
+            }}
+          >
+            <UnorderedList mt={2}>
+              {job.description.map((desc) => (
+                <ListItem key={desc} style={{ wordBreak: 'break-word' }}>
+                  {desc}
+                </ListItem>
+              ))}
+            </UnorderedList>
+          </Grid>
+        </Container>
       </Grid>
-    </Grid>
+    </Container>
   );
 };
 
@@ -121,45 +124,60 @@ export default function ExperienceOverview() {
   };
 
   return (
-    <Flex
-      as={Grid}
-      container
-      item
-      direction={{ base: 'column', md: 'row' }}
-      justifyContent="center"
-      alignItems={{ base: 'center', md: 'flex-start' }}
-      xs={12}
-      sm={11}
-      md={10}
-      borderRadius="lg"
-      {...borderConfig()}
-      rounded="md"
-      spacing={8}
+    <Container
       style={{
+        padding: 0,
+        // maxWidth: { sm: '70%', md: '900px' },
         margin: 'auto',
-        maxWidth: { md: '900px', sm: '70vw' },
-        width: { md: '100%', sm: '70%' },
       }}
     >
-      <Grid
+      <Flex
+        as={Grid}
         container
-        direction="column"
         item
+        direction={{ base: 'column', md: 'row' }}
+        justifyContent="center"
+        alignItems={{ base: 'center', md: 'flex-start' }}
         xs={12}
-        sm={12}
-        md={4}
-        alignItems="center"
-        justifyContent="flex-start"
+        sm={11}
+        md={10}
+        borderRadius="lg"
+        {...borderConfig()}
+        rounded="md"
+        spacing={8}
+        style={{
+          margin: 'auto',
+          width: '100%',
+        }}
       >
-        {showSelect ? (
-          <ExperienceButtons expIndex={index} setIndex={setIndex} jobs={jobs} />
-        ) : (
-          <ExperienceSelect expIndex={index} setIndex={setIndex} jobs={jobs} />
-        )}
-      </Grid>
-      <Grid container item xs={12} sm={12} md={8}>
-        <ExperienceDetails index={index} jobs={jobs} />
-      </Grid>
-    </Flex>
+        <Grid
+          container
+          direction="column"
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          alignItems="center"
+          justifyContent="flex-start"
+        >
+          {showSelect ? (
+            <ExperienceButtons
+              expIndex={index}
+              setIndex={setIndex}
+              jobs={jobs}
+            />
+          ) : (
+            <ExperienceSelect
+              expIndex={index}
+              setIndex={setIndex}
+              jobs={jobs}
+            />
+          )}
+        </Grid>
+        <Grid container item xs={12} sm={12} md={8} maxWidth="680px">
+          <ExperienceDetails index={index} jobs={jobs} />
+        </Grid>
+      </Flex>
+    </Container>
   );
 }
