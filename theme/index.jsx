fa5092330c846ac40b39c2488f8efb6ef3a06723
Filipe <filipelehmannp@gmail.com>
2025-09-10 00:35:34 -0300

    fix: inter

--- theme/index.jsx
+++ theme/index.jsx
@@ -37,9 +37,9 @@ export const colors = {
 };
 
 const fonts = {
-  body: 'Nunito, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+  body: 'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
   heading:
-    'Nunito, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+    'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
 };
 
 export default extendTheme({
